63a213d794221e412b04f767d06e98eadaf358ff
matfuentesv <mat.fuentes@duocuc.cl>
2024-04-23 18:56:33 -0400

    target

--- src/main/java/cl/company/ecommerce/controller/EcommerceController.java
+++ src/main/java/cl/company/ecommerce/controller/EcommerceController.java
@@ -22,7 +22,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-//@RequestMapping("/api")
+@RequestMapping("/api")
 public class EcommerceController {
 
     private static final Logger log = LoggerFactory.getLogger(EcommerceController.class);
